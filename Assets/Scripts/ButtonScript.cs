0e8e48a13659026021be5b2626efc94e9b4743db
LeviHerring <514121@escg.ac.uk>
2021-09-29 13:58:07 +0100

    wegiugftseaiugfwiugtfiwu

---
+++ Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScript : MonoBehaviour
+{
+    bool isClicked = false;
+    int turn = 1;
+
+    public GameObject cross;
+
+    public GameObject naught; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClick()
+    {
+        Debug.Log("Button Pressed");
+        isClicked = true; 
+
+        if(turn%2 == 0)
+        {
+            Instantiate(cross);
+        }
+        else
+        {
+           
+            Instantiate(naught);
+        }
+        isClicked = false;
+        turn = turn + 1; 
+        
+    }
+
+}
---
+++ Assets/Scripts/FightingButtonScript.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FightingButtonScript : MonoBehaviour
+{
+
+    bool isClicked = false;
+    int turn = 1; 
+
+    public GameObject enemyText;
+    public GameObject HeroText; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClickAttack()
+    {
+        isClicked = true; 
+
+        if(turn%2 == 0)
+        {
+            enemyText.SetActive(false);
+            HeroText.SetActive(true);
+        }
+        else
+        {
+            enemyText.SetActive(true);
+            HeroText.SetActive(false);
+        }
+        isClicked = false;
+        turn = turn + 1;
+
+    }
+
+    public void OnClickHeal()
+    {
+
+    }
+
+    public void OnClickRun()
+    {
+
+    }
+
+}
